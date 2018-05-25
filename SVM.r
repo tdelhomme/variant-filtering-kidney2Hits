2aa65031a35236543acad2d1a52882b86b326d67
tdelhomme <delhommet@students.iarc.fr>
2018-05-25 11:08:16 +0200

    run svm

--- SVM.r
+++ SVM.r
@@ -1,11 +1,8 @@
 library(e1071)
 
-train_table$status = as.factor(train_table$status)
-
-model_svm <- svm(status ~ . , data=train_table[,my_features])
+model_svm <- svm(as.factor(status) ~ . , data=train_table[,my_features])
 
 pred = predict(model_svm, train_table[,my_features])
-
 
 sens_svm = sum(train_table$status=="TP" & pred=="TP") / sum(train_table$status=="TP")
 TDR_svm = sum(train_table$status == "TP" & pred == "TP") / sum(pred == "TP")
@@ -24,11 +21,8 @@ for(i in 1:10){
   propTP = as.numeric(table(train$status)["TP"] / nrow(train))
   propFP = as.numeric(table(train$status)["FP"] / nrow(train))
   
-  rf_fold = randomForest(as.factor(status) ~ .,
-                         data = train[,my_features],
-                         importance = TRUE, # to allow us to inspect variable importance
-                         ntree = 500, sampsize = as.numeric(table(train$status)["TP"])) #classwt = c(propTP, propFP))
-  test$prediction = predict(rf_fold, test)
+  svm_fold = svm(as.factor(status) ~ . , data=train[,my_features])
+  test$prediction = predict(svm_fold, test[,my_features])
   kfold_sens = c(kfold_sens, (sum(test$status == "TP" & test$prediction == "TP") / sum(test$status == "TP")) )
   kfold_spec = c(kfold_spec, (sum(test$status == "FP" & test$prediction == "FP") / sum(test$status == "FP")) )
   kfold_TDR = c(kfold_TDR, (sum(test$status == "TP" & test$prediction == "TP") / sum(test$prediction == "TP")) )
