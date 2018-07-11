64fa255665934eb475cc689b4705cf5ae3cac87d
tdelhomme <delhommet@students.iarc.fr>
2018-07-11 14:15:11 +0200

    allow both snvs and indels models

--- random_forest.r
+++ random_forest.r
@@ -7,8 +7,15 @@ train_table="tables/K2H_AllVariants_All_Lib_NoMinAF_noequal_GOF_addVCFfeatures_i
 train_table = read.table(train_table, quote="\"", stringsAsFactors=F, sep="\t", header=T)
 
 train_table$IoD = 1 + train_table$SIG * 10^(train_table$ERR)
-train_table = train_table[which(train_table$TYPE_INFO=="snv"),]
-#train_table = train_table[which(train_table$TYPE_INFO=="ins" | train_table$TYPE_INFO=="del"),]
+
+type="snv"
+if(type=="snv"){
+  train_table = train_table[which(train_table$TYPE_INFO=="snv"),]  
+}
+if(type=="indel"){
+  train_table = train_table[which(train_table$TYPE_INFO=="ins" | train_table$TYPE_INFO=="del"),]  
+}
+
 train_table[which(is.infinite(train_table$MIN_DIST)),"MIN_DIST"] = 1000000000
 train_table[which(is.infinite(train_table$MaxRatioWin)),"MaxRatioWin"] = 1000000000
 
@@ -71,9 +78,17 @@ for(i in 1:10){
   kfold_TDR = c(kfold_TDR, (sum(test$status == "TP" & test$prediction == "TP") / sum(test$prediction == "TP")) )
 }
 
-boxplot(data.frame("TDR"=kfold_TDR, "sensitivity"=kfold_sens), col="lightgrey", ylim=c(0.85,1), outpch=20, outcex=0.75, axes=F, staplelwd=2)
+if(type=="snv") { boxplot(data.frame("TDR"=kfold_TDR, "sensitivity"=kfold_sens), col="lightgrey", ylim=c(0.85,1), outpch=20, 
+        outcex=0.75, axes=F, staplelwd=2, main=type)
 axis(side=3, at=c(1,2), labels=c("TDR","sensitivity"), col = NA, col.ticks = NA)
-axis(side=2, at=c("0.85","0.90","0.95","1"))
+axis(side=2, at=c("0.85","0.90","0.95","1")) }
+
+if(type=="indel") { boxplot(data.frame("TDR"=kfold_TDR, "sensitivity"=kfold_sens), col="lightgrey", ylim=c(0,1), outpch=20, 
+                          outcex=0.75, axes=F, staplelwd=2, main=type)
+  axis(side=3, at=c(1,2), labels=c("TDR","sensitivity"), col = NA, col.ticks = NA)
+  axis(side=2, at=seq(0,1,by=0.1)) }
+
+# load functions from MLP.r and run plot_ggviolin(kfold_TDR, kfold_sens, type)
 
 # compare with home filters
 train_table$prediction_home_filters = "FP"
@@ -84,7 +99,9 @@ TDR_home_filters = sum(train_table$status == "TP" & train_table$prediction_home_
 points(1, TDR_home_filters, pch=8, col="darkred", lwd=2)
 points(2, sens_home_filter, pch=8, col="darkred", lwd=2)
 
-legend(x=2, y=0.88, c("AF>0.1","RVSB<0.95","DP>50","ERR<0.01"), col=c("darkred","white","white","white"), pch=8, bty="n", cex=0.75)
+legend(x=2, y=0.88, c("AF>0.1","RVSB<0.95","DP>50","ERR<0.01"), col=c("black","white","white","white"), pch=4, bty="n", cex=0.75)
 
 sens_rf = sum(train_table$status=="TP" & train_table$prediction=="TP") / sum(train_table$status=="TP")
 TDR_rf = sum(train_table$status == "TP" & train_table$prediction == "TP") / sum(train_table$prediction == "TP")
+
+
